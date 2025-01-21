855098d8e26f9d7e60e7177965b771a12ddadbe2
Mayaqq <i@mayaqq.dev>
2025-01-21 10:25:40 +0100

    More messages

--- src/client/kotlin/dev/mayaqq/skyblock/client/chat/Messages.kt
+++ src/client/kotlin/dev/mayaqq/skyblock/client/chat/Messages.kt
@@ -6,24 +6,45 @@ import java.util.function.Supplier
 enum class Messages(val regex: Regex, val option: Supplier<HidingOption>) {
     // Lobby
     LOBBY_JOIN(Regex("""(?:>>> )?[\s\S]*joined the lobby!(?: <<<)?"""), { ChatConfig.lobbyJoinMessage }),
+    // Ability
+    PREVIOUS_ABILITY_REMOVED(Regex("""Your previous [\s\S]* was removed!"""), { ChatConfig.previousAbilityRemoved }),
     // System
-    GEXP_GAIN(Regex("""You earned \\d+ GEXP from playing [\s\S]*!"""), { ChatConfig.gexpGain }),
+    GEXP_GAIN(Regex("""You earned \d+ GEXP from playing [\s\S]*!"""), { ChatConfig.gexpGain }),
     PROFILE_ID(Regex("""Profile ID: [\s\S]*"""), { ChatConfig.profileId }),
     PLAYING_ON_PROFILE(Regex("""You are playing on profile: [\s\S]*"""), { ChatConfig.playingOnProfile }),
     WATCHDOG_REPORT(Regex("""[\s\S]*(\\\[WATCHDOG ANNOUNCEMENT]|Watchdog has banned|Staff have banned an additional|Blacklisted modifications are a bannable offense!)[\s\S]*"""), { ChatConfig.watchdogReport }),
     // Skyblock
     SKYBLOCK_WELCOME(Regex("""Welcome to Hypixel SkyBlock!"""), { ChatConfig.welcomeToSkyblock }),
     FIRE_SALE(Regex("""A FIRE SALE [\s\S]*"""), { ChatConfig.fireSale }),
+    ALLOWANCE(Regex("""ALLOWANCE! You earned [\s\S]* coins!"""), { ChatConfig.allowance }),
+    NPC(Regex("""\\\[NPC] [\s\S]*"""), { ChatConfig.npc }),
+    NPC_BUY(Regex("""You bought [\s\S]* x\d+ for [\d.]+[\dA-Za-z]* Coins!"""), { ChatConfig.npcBuy }),
+    BUY_LIMIT(Regex("""You may only buy up to [\s\S]* of this item each day!"""), { ChatConfig.buyLimit }),
     // Travel
     WARPING(Regex("""Warping..."""), { ChatConfig.warping }),
     SENDING_TO_SERVER(Regex("""Sending to server [\s\S]*"""), { ChatConfig.sendingToServer }),
+    UNKNOWN_DESTINATION(Regex("""Unknown destination! Check the Fast Travel menu to view options!"""), { ChatConfig.unknownDestination }),
     // Dungeon Hub
     DUNGEON_REWARD(Regex("""RARE REWARD! .+ found a .+ in their .+!"""), { ChatConfig.dungeonReward }),
+    // Dungeon
+    CANNOT_USE_OUTSIDE_DUNGEON(Regex("""You can only use this item in dungeons!"""), { ChatConfig.cannotUseOutsideDungeon }),
+    QUEUING(Regex("""Queuing... \\(Attempt [\s\S]{3}\\)"""), { ChatConfig.queuing }),
+    STARTING(Regex("""Starting in \d second(s)?."""), { ChatConfig.starting }),
+    STATS_DOUBLED(Regex("""Your [\s\S]* stats are doubled because you are the only player using this class!"""), { ChatConfig.statsDoubled }),
+    CLASS_MESSAGE(Regex("""\[(Mage|Healer|Berserk|Archer|Tank)] [\s\S]*"""), { ChatConfig.classMessage }),
+    DUNGEON_REQUESTED_ALREADY(Regex("""You have already requested a server! Please wait a bit."""), { ChatConfig.requestedDungeonAlready }),
+    SELECTED_CLASS_MESSAGE(Regex("""You have selected the [\s\S]* Dungeon Class!"""), { ChatConfig.selectedClassMessage }),
+    // Mining
+    WIND_CHANGED_DIRECTION(Regex("""The wind has changed direction!"""), { ChatConfig.windChangedDirection }),
+    EVENT_STARTING(Regex("""[\s\S]{3}The [\s\S]* event starts in 20 seconds![\s\S]*"""), { ChatConfig.eventStarting }),
+    FALLEN_STAR(Regex("""✯ A Fallen Star has crashed at Cliffside Veins![\s\S]*"""), { ChatConfig.FallenStar }),
     // Item
     CANNOT_USE_ITEM(Regex("""Cannot use this item here!"""), { ChatConfig.cannotUseItem }),
     ABILITY_COOLDOWN(Regex("""Ability is on cooldown for [\s\S]*"""), { ChatConfig.abilityCooldown }),
     ZOMBIE_SWORD_NO_MORE_CHARGES(Regex("""No more charges, next one in [\s\S]*"""), { ChatConfig.zombieSwordNoMoreCharges }),
     // Miscellaneous
     RINGING(Regex("""✆ RING...[\s\S]*"""), { ChatConfig.ringing }),
-    ALREADY_FOUND_FAIRY_SOUL(Regex("""You have already found that Fairy Soul!"""), { ChatConfig.alreadyFoundFairySoul });
+    ALREADY_FOUND_FAIRY_SOUL(Regex("""You have already found that Fairy Soul!"""), { ChatConfig.alreadyFoundFairySoul }),
+    // Ironman
+    AUCTION_DISALLOWED(Regex("""You can't use the Auction House while playing on your current profile type!"""), { ChatConfig.auctionDisallowed }),
 }
--- src/client/kotlin/dev/mayaqq/skyblock/client/config/categories/ChatConfig.kt
+++ src/client/kotlin/dev/mayaqq/skyblock/client/config/categories/ChatConfig.kt
@@ -1,20 +1,19 @@
 package dev.mayaqq.skyblock.client.config.categories
 
 import com.teamresourceful.resourcefulconfig.api.annotations.Category
-import com.teamresourceful.resourcefulconfig.api.annotations.Comment
 import com.teamresourceful.resourcefulconfig.api.annotations.ConfigEntry
 import com.teamresourceful.resourcefulconfig.api.annotations.ConfigInfo
-import com.teamresourceful.resourcefulconfig.api.annotations.ConfigOption
+import com.teamresourceful.resourcefulconfig.api.annotations.ConfigOption.Separator
 import dev.mayaqq.skyblock.client.chat.HidingOption
 
 @ConfigInfo(titleTranslation = "config.skyblock.chat.title")
 @Category("chat")
 object ChatConfig {
-    @ConfigOption.Separator("Lobby")
+    @Separator("Lobby")
     @ConfigEntry(id = "lobbyJoinMessage", translation = "config.skyblock.chat.lobby.lobbyJoinMessage")
     var lobbyJoinMessage = HidingOption.DISABLED
 
-    @ConfigOption.Separator("System")
+    @Separator("System")
     @ConfigEntry(id = "gexpGain", translation = "config.skyblock.chat.system.gexpGain")
     var gexpGain = HidingOption.DISABLED
     @ConfigEntry(id = "profileId", translation = "config.skyblock.chat.system.profileId")
@@ -24,27 +23,63 @@ object ChatConfig {
     @ConfigEntry(id = "watchdogReport", translation = "config.skyblock.chat.system.watchdogReport")
     var watchdogReport = HidingOption.DISABLED
 
-    @ConfigOption.Separator("Action Bar")
+    @Separator("Action Bar")
     @ConfigEntry(id = "abilityUse", translation = "config.skyblock.chat.actionBar.abilityUse")
     var abilityUse = false
+    @ConfigEntry(id = "previousAbilityRemoved", translation = "config.skyblock.chat.actionBar.previousAbilityRemoved")
+    var previousAbilityRemoved = HidingOption.DISABLED
 
-    @ConfigOption.Separator("Skyblock")
+    @Separator("Skyblock")
     @ConfigEntry(id = "welcomeToSkyblock", translation = "config.skyblock.chat.welcomeToSkyblock")
     var welcomeToSkyblock = HidingOption.DISABLED
     @ConfigEntry(id = "fireSale", translation = "config.skyblock.chat.fireSale")
     var fireSale = HidingOption.DISABLED
+    @ConfigEntry(id = "allowance", translation = "config.skyblock.chat.allowance")
+    var allowance = HidingOption.DISABLED
+    @ConfigEntry(id = "npc", translation = "config.skyblock.chat.npc")
+    var npc = HidingOption.DISABLED
+    @ConfigEntry(id = "npcBuy", translation = "config.skyblock.chat.npcBuy")
+    var npcBuy = HidingOption.DISABLED
+    @ConfigEntry(id = "buyLimit", translation = "config.skyblock.chat.buyLimit")
+    var buyLimit = HidingOption.DISABLED
 
-    @ConfigOption.Separator("Travel")
+    @Separator("Travel")
     @ConfigEntry(id = "warping", translation = "config.skyblock.chat.travel.warping")
     var warping = HidingOption.DISABLED
     @ConfigEntry(id = "sendingToServer", translation = "config.skyblock.chat.travel.sendingToServer")
     var sendingToServer = HidingOption.DISABLED
+    @ConfigEntry(id = "unknownDestination", translation = "config.skyblock.chat.travel.unknownDestination")
+    var unknownDestination = HidingOption.DISABLED
 
-    @ConfigOption.Separator("Dungeon Hub")
+    @Separator("Dungeon Hub")
     @ConfigEntry(id = "dungeonReward", translation = "config.skyblock.chat.dungeonHub.dungeonReward")
     var dungeonReward = HidingOption.DISABLED
 
-    @ConfigOption.Separator("Item")
+    @Separator("Dungeon")
+    @ConfigEntry(id = "cannotUseOutsideDungeon", translation = "config.skyblock.chat.dungeon.cannotUseOutsideDungeon")
+    var cannotUseOutsideDungeon = HidingOption.DISABLED
+    @ConfigEntry(id = "queuing", translation = "config.skyblock.chat.dungeon.queuing")
+    var queuing = HidingOption.DISABLED
+    @ConfigEntry(id = "starting", translation = "config.skyblock.chat.dungeon.starting")
+    var starting = HidingOption.DISABLED
+    @ConfigEntry(id = "statsDoubled", translation = "config.skyblock.chat.dungeon.statsDoubled")
+    var statsDoubled = HidingOption.DISABLED
+    @ConfigEntry(id = "classMessage", translation = "config.skyblock.chat.dungeon.classMessage")
+    var classMessage = HidingOption.DISABLED
+    @ConfigEntry(id = "requestedDungeonAlready", translation = "config.skyblock.chat.dungeon.requestedDungeonAlready")
+    var requestedDungeonAlready = HidingOption.DISABLED
+    @ConfigEntry(id = "selectedClassMessage", translation = "config.skyblock.chat.dungeon.selectedClassMessage")
+    var selectedClassMessage = HidingOption.DISABLED
+
+    @Separator("Mining")
+    @ConfigEntry(id = "windChangedDirection", translation = "config.skyblock.chat.mining.windChangedDirection")
+    var windChangedDirection = HidingOption.DISABLED
+    @ConfigEntry(id = "eventStarting", translation = "config.skyblock.chat.mining.eventStarting")
+    var eventStarting = HidingOption.DISABLED
+    @ConfigEntry(id = "FallenStar", translation = "config.skyblock.chat.mining.FallenStar")
+    var FallenStar = HidingOption.DISABLED
+
+    @Separator("Item")
     @ConfigEntry(id = "cannotUseItem", translation = "config.skyblock.chat.item.cannotUseItem")
     var cannotUseItem = HidingOption.DISABLED
     @ConfigEntry(id = "abilityCooldown", translation = "config.skyblock.chat.item.abilityCooldown")
@@ -52,9 +87,13 @@ object ChatConfig {
     @ConfigEntry(id = "zombieSwordNoMoreCharges", translation = "config.skyblock.chat.item.zombieSwordNoMoreCharges")
     var zombieSwordNoMoreCharges = HidingOption.DISABLED
 
-    @ConfigOption.Separator("Miscellaneous")
+    @Separator("Miscellaneous")
     @ConfigEntry(id = "ringing", translation = "config.skyblock.chat.misc.ringing")
     var ringing = HidingOption.DISABLED
     @ConfigEntry(id = "alreadyFoundFairySoul", translation = "config.skyblock.chat.misc.alreadyFoundFairySoul")
     var alreadyFoundFairySoul = HidingOption.DISABLED
+
+    @Separator("Ironman")
+    @ConfigEntry(id = "auctionDisallowed", translation = "config.skyblock.chat.ironman.auctionDisallowed")
+    var auctionDisallowed = HidingOption.DISABLED
 }
